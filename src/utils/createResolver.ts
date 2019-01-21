fdba3c81a7bf830f00755b42577f1b7d237c1981
Jasenpan1987 <jasenpan@gmail.com>
2019-01-22 02:35:04 +1100

    add middleware

--- src/utils/createResolver.ts
+++ src/utils/createResolver.ts
@@ -1,16 +1,25 @@
 // // // this is a demo for higher order resolvers
 // // // to see how to use, checkout the CreateUser.ts and the CreateProduct.ts
 
-import { Resolver, Mutation, Arg, ClassType } from "type-graphql";
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  ClassType,
+  UseMiddleware
+} from "type-graphql";
+import { Middleware } from "type-graphql/interfaces/Middleware";
 
 export function createResolver<CT extends ClassType, IT extends ClassType>(
   suffix: string,
   returnTypeCls: CT,
   inputTypeCls: IT,
-  entity: any
+  entity: any,
+  middlewares?: Middleware<any>[]
 ) {
   @Resolver({ isAbstract: true })
   abstract class BaseResolver {
+    @UseMiddleware(...(middlewares || [])) // if you want to use @Authorized, use authorize middleware instead, because we can't add decorator at runtime.
     @Mutation(() => returnTypeCls, { name: `create${suffix}` })
     async createUser(
       @Arg("data", () => inputTypeCls) data: typeof inputTypeCls
